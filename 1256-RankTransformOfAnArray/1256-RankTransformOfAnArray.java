11697aca302c0727ae0be3e819fe8bd3a31485ae
Harsh <135042421+H2rsh16@users.noreply.github.com>
2026-05-22 11:22:24 +0530

    Create 1256-RankTransformOfAnArray

---
+++ 1256-RankTransformOfAnArray/1256-RankTransformOfAnArray.java
@@ -0,0 +1,17 @@
+// Last updated: 22/05/2026, 11:22:23
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] sorted = Arrays.stream(arr).distinct().sorted().toArray();
+
+        for(int i = 0; i < sorted.length; i++){
+            map.put(sorted[i], i + 1);
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = map.get(arr[i]);
+        }
+
+        return arr;
+    }
+}
