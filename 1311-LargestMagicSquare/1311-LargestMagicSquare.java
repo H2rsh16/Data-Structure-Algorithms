d878bc9a0c4e654604e0a96ca25cbcbf3bfd02b5
Harsh <135042421+H2rsh16@users.noreply.github.com>
2026-05-22 11:22:17 +0530

    Create 1311-LargestMagicSquare

---
+++ 1311-LargestMagicSquare/1311-LargestMagicSquare.java
@@ -0,0 +1,54 @@
+// Last updated: 22/05/2026, 11:22:16
+class Solution {
+    public int largestMagicSquare(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[][] row = new int[m][n + 1];
+        int[][] col = new int[m + 1][n];
+        int[][] diag1 = new int[m + 1][n + 1];
+        int[][] diag2 = new int[m + 1][n + 1];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                row[i][j + 1] = row[i][j] + grid[i][j];
+                col[i + 1][j] = col[i][j] + grid[i][j];
+                diag1[i + 1][j + 1] = diag1[i][j] + grid[i][j];
+                diag2[i + 1][j] = diag2[i][j + 1] + grid[i][j];
+            }
+        }
+
+        int maxSize = Math.min(m, n);
+
+        for (int k = maxSize; k >= 1; k--) {
+            for (int i = 0; i + k <= m; i++) {
+                for (int j = 0; j + k <= n; j++) {
+                    if (isMagic(i, j, k, row, col, diag1, diag2)) {
+                        return k;
+                    }
+                }
+            }
+        }
+        return 1;
+    }
+
+    private boolean isMagic(int r, int c, int k,
+                            int[][] row, int[][] col,
+                            int[][] diag1, int[][] diag2) {
+
+        int target = row[r][c + k] - row[r][c];
+
+        for (int i = r; i < r + k; i++) {
+            if (row[i][c + k] - row[i][c] != target) return false;
+        }
+
+        for (int j = c; j < c + k; j++) {
+            if (col[r + k][j] - col[r][j] != target) return false;
+        }
+
+        int d1 = diag1[r + k][c + k] - diag1[r][c];
+        int d2 = diag2[r + k][c] - diag2[r][c + k];
+
+        return d1 == target && d2 == target;
+    }
+}
