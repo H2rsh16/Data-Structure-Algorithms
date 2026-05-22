8a829bb89e01d47fbfd8b4cacec8624e6fed8ba7
Harsh <135042421+H2rsh16@users.noreply.github.com>
2026-05-22 11:19:34 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,15 @@
+// Last updated: 22/05/2026, 11:19:33
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
