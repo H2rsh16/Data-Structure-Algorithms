5b726f44f696da9e57e00174d345bb811a64d701
Harsh <135042421+H2rsh16@users.noreply.github.com>
2026-05-22 11:19:07 +0530

    Create 3496-MinimumNumberOfSecondsToMakeMountainHeightZero

---
+++ 3496-MinimumNumberOfSecondsToMakeMountainHeightZero/3496-MinimumNumberOfSecondsToMakeMountainHeightZero.java
@@ -0,0 +1,37 @@
+// Last updated: 22/05/2026, 11:19:06
+class Solution {
+
+    public long minNumberOfSeconds(int mountainHeight, int[] workerTimes) {
+
+        long left = 0;
+        long right = (long)1e18;
+        long ans = right;
+
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+
+            if (canFinish(mid, mountainHeight, workerTimes)) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    private boolean canFinish(long time, int mountainHeight, int[] workerTimes) {
+        long total = 0;
+
+        for (int t : workerTimes) {
+
+            long val = (long)(Math.sqrt(1 + (8.0 * time) / t) - 1) / 2;
+            total += val;
+
+            if (total >= mountainHeight) return true;
+        }
+
+        return total >= mountainHeight;
+    }
+}
