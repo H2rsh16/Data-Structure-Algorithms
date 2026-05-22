74ddee4224cf6094bdee909b5b5c497ef2a85b53
Harsh <135042421+H2rsh16@users.noreply.github.com>
2026-05-22 11:20:01 +0530

    Create 2521-PathsInMatrixWhoseSumIsDivisibleByK

---
+++ 2521-PathsInMatrixWhoseSumIsDivisibleByK/2521-PathsInMatrixWhoseSumIsDivisibleByK.java
@@ -0,0 +1,32 @@
+// Last updated: 22/05/2026, 11:20:00
+class Solution {
+    public int numberOfPaths(int[][] grid, int k) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int MOD = 1_000_000_007;
+
+        int[][][] dp = new int[m][n][k];
+
+        dp[0][0][grid[0][0] % k] = 1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int r = 0; r < k; r++) {
+                    int val = grid[i][j];
+
+                    if (i > 0) {
+                        int prevR = (r - val % k + k) % k;
+                        dp[i][j][r] = (dp[i][j][r] + dp[i - 1][j][prevR]) % MOD;
+                    }
+                    
+                    if (j > 0) {
+                        int prevR = (r - val % k + k) % k;
+                        dp[i][j][r] = (dp[i][j][r] + dp[i][j - 1][prevR]) % MOD;
+                    }
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1][0];
+    }
+}
