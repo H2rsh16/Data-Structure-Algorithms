520b0414f9e69b5ec26b0fbd4b54b494a4a2d591
Harsh <135042421+H2rsh16@users.noreply.github.com>
2026-05-22 11:19:31 +0530

    Create 2892-CheckIfArrayIsGood

---
+++ 2892-CheckIfArrayIsGood/2892-CheckIfArrayIsGood.java
@@ -0,0 +1,20 @@
+// Last updated: 22/05/2026, 11:19:30
+class Solution {
+    public boolean isGood(int[] nums) {
+        Arrays.sort(nums);
+
+        int n = nums.length;
+
+        if (nums[n - 1] != n - 1) {
+            return false;
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] != i + 1) {
+                return false;
+            }
+        }
+
+        return nums[n - 1] == nums[n - 2];
+    }
+}
