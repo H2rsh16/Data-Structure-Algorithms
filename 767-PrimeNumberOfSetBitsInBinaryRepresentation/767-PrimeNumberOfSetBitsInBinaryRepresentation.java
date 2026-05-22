d4434992c3ef9f189fb294479d16b45ef236084f
Harsh <135042421+H2rsh16@users.noreply.github.com>
2026-05-22 11:23:06 +0530

    Create 767-PrimeNumberOfSetBitsInBinaryRepresentation

---
+++ 767-PrimeNumberOfSetBitsInBinaryRepresentation/767-PrimeNumberOfSetBitsInBinaryRepresentation.java
@@ -0,0 +1,23 @@
+// Last updated: 22/05/2026, 11:23:05
+class Solution {
+    private boolean isPrime(int n){
+        if(n < 2) return false;
+
+        for(int i = 2; i * i <= n; i++){
+            if(n % i == 0) return false;
+        }
+
+        return true;
+    }
+    public int countPrimeSetBits(int left, int right) {
+        int count = 0;
+
+        for(int i = left; i <= right; i++){
+            int setBits = Integer.bitCount(i);
+
+            if(isPrime(setBits)) count++;
+        }
+
+        return count;
+    }
+}
