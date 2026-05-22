ee8f8353a3cec5ee6db88a7f6e2e834beec93079
Harsh <135042421+H2rsh16@users.noreply.github.com>
2026-05-22 11:20:45 +0530

    Create 1988-MinimizeMaximumPairSumInArray

---
+++ 1988-MinimizeMaximumPairSumInArray/1988-MinimizeMaximumPairSumInArray.java
@@ -0,0 +1,18 @@
+// Last updated: 22/05/2026, 11:20:44
+class Solution {
+    public int minPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int max = 0;
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left < right){
+            int sum = nums[left] + nums[right];
+            max = Math.max(max, sum);
+            left++;
+            right--;
+        }
+
+        return max;
+    }
+}
