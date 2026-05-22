13dac3929f5ee7bf23504c6d1f96b81321ef9c80
Harsh <135042421+H2rsh16@users.noreply.github.com>
2026-05-22 11:19:13 +0530

    Create 3462-VowelsGameInAString

---
+++ 3462-VowelsGameInAString/3462-VowelsGameInAString.java
@@ -0,0 +1,12 @@
+// Last updated: 22/05/2026, 11:19:13
+class Solution {
+    public boolean doesAliceWin(String s) {
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+
+            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') return true;
+        }
+
+        return false;
+    }
+}
