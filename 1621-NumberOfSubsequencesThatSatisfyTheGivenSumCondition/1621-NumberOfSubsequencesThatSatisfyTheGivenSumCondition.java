277e37301cf4533421ae5a50b15bf3eb7ed24a22
Harsh <135042421+H2rsh16@users.noreply.github.com>
2026-05-22 11:21:41 +0530

    Create 1621-NumberOfSubsequencesThatSatisfyTheGivenSumCondition

---
+++ 1621-NumberOfSubsequencesThatSatisfyTheGivenSumCondition/1621-NumberOfSubsequencesThatSatisfyTheGivenSumCondition.java
@@ -0,0 +1,28 @@
+// Last updated: 22/05/2026, 11:21:40
+class Solution {
+    private static final int MOD = 1_000_000_007;
+    public int numSubseq(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int[] pow2 = new int[n];
+        pow2[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            pow2[i] = (pow2[i - 1] * 2) % MOD;
+        }
+
+        int left = 0, right = n - 1;
+        int result = 0;
+
+        while (left <= right) {
+            if (nums[left] + nums[right] <= target) {
+                result = (result + pow2[right - left]) % MOD;
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return result;
+    }
+}
