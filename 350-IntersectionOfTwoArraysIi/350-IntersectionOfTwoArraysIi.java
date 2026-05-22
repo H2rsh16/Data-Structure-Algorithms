e35914683605aa83c15e0acced8c1be8c6180b4d
Harsh <135042421+H2rsh16@users.noreply.github.com>
2026-05-22 11:24:14 +0530

    Create 350-IntersectionOfTwoArraysIi

---
+++ 350-IntersectionOfTwoArraysIi/350-IntersectionOfTwoArraysIi.java
@@ -0,0 +1,26 @@
+// Last updated: 22/05/2026, 11:24:14
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+
+        int i = 0, j = 0, k = 0;
+
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        while(i < m && j < n){
+            if(nums1[i] < nums2[j]){
+                i++;
+            }
+            else if(nums1[i] > nums2[j]){
+                j++;
+            }
+            else{
+                nums1[k++] = nums1[i++];
+                j++;
+            }
+        }
+        return Arrays.copyOfRange(nums1, 0, k);
+    }
+}
